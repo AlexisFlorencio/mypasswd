ef477a0f3307b8137c4ccb3f3ab6f15515a00a7c
AlexisFlorencio <15586163.clot@fje.edu>
2024-05-06 10:28:24 +0200

    passwdremover3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import PyPDF2
 import io
+import tempfile
 
 def remove_pdf_password(input_pdf, output_pdf, password):
     with open(input_pdf, 'rb') as file:
@@ -22,8 +24,11 @@ uploaded_file = st.file_uploader("Carga el PDF con contraseña", type="pdf")
 if uploaded_file is not None:
     password = st.text_input("Introduce la contraseña del PDF")
     if st.button("Eliminar contraseña"):
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            input_pdf = temp_file.name
         output_pdf = f"unlocked_{uploaded_file.name}"
-        if remove_pdf_password(uploaded_file, output_pdf, password):
+        if remove_pdf_password(input_pdf, output_pdf, password):
             st.success(f"¡Contraseña eliminada! Puedes descargar el PDF sin contraseña desde [este enlace]({output_pdf}).")
         else:
             st.error("El PDF no tiene contraseña o la contraseña proporcionada es incorrecta.")
